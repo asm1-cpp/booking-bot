287f794d2ce0f1edfbfd44d304325a76713d1d5f
asm1-cpp <spinach-diary-duo@duck.com>
2026-07-02 23:31:22 +0500

    Initial commit

---
+++ bot/keyboards/zone_menu_keyboard.py
@@ -0,0 +1,12 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from content import BUTTONS2
+
+def get_hello_menu_keyboard():
+    builder = InlineKeyboardBuilder()
+
+    for callback_data, text in BUTTONS2.items():
+        builder.button(text=text, callback_data=callback_data)
+
+    builder.adjust(2,2)
+
+    return builder.as_markup()
